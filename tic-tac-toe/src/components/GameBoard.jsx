a5a904133d8ed68703a00573438150d6f763560b
Hao Truong <haotr@Haos-MBP.lan>
2024-07-05 21:38:18 -0700

    added click handle

--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 const inititalGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(inititalGameBoard);
+
+  function handleSelectSquare(rowIdx, colIdx) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIdx][colIdx] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {inititalGameBoard.map((row, rowIdx) => (
+      {gameBoard.map((row, rowIdx) => (
         <li key={rowIdx}>
           <ol>
             {row.map((playerSymbol, colIdx) => (
               <li key={colIdx}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIdx, colIdx)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
